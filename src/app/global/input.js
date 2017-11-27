32505c9e05c878fc8bc904567464037e2f3829b2
whistlemm <276158636@qq.com>
2017-11-28 01:24:03 +0800

    [feature] custome input component

---
+++ src/app/global/input.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Flex, Icon } from 'antd-mobile'
+import Style from './input.less'
+
+export default class Input extends React.Component {
+  static propTypes = {
+    value: PropTypes.any,
+    defaultValue: PropTypes.any,
+    prefix: PropTypes.node,
+    suffix: PropTypes.node
+  }
+
+  constructor (props) {
+    super(props)
+  }
+
+  focus () {
+    this.refs.input.focus()
+  }
+  blur () {
+    this.refs.input.blur()
+  }
+  render () {
+    const props = {...this.props}
+
+    if ('value' in props) {
+      props.value = props.value || ''
+      delete props.defaultValue
+    }
+
+    const prefixNode = props.prefix
+    const suffixNode = props.suffix
+
+    delete props.prefix
+    delete props.suffix
+
+    return (
+      <div className={Style.wrapper}>
+        { prefixNode }
+        <input
+          ref="input"
+          className={Style.input}
+          {...props}
+        />
+        { suffixNode }
+      </div>
+    )
+  }
+}
