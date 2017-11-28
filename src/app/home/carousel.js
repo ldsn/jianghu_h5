f495965555c34c318d081accfd86b9f7e94fb217
linhan <15726044837@163.com>
2017-11-28 19:38:08 +0800

    跑马灯 (#3)
    * 添加走马灯
    
    * 小修改

--- src/app/home/carousel.js
+++ src/app/home/carousel.js
@@ -1,10 +1,69 @@
 import React from 'react'
+import { Carousel } from 'antd-mobile'
+import Style from './css/car.less'
+
+const tempData = [
+  {
+    imgs: 'AiyWuByWklrrUDlFignR',
+    url: 'https://zos.alipayobjects.com/rmsportal/',
+    linkUrl: 'https://www.baidu.com'
+  },
+  {
+    imgs: 'AiyWuByWklrrUDlFignR',
+    url: 'https://zos.alipayobjects.com/rmsportal/',
+    linkUrl: 'https://www.baidu.com'
+  },
+  {
+    imgs: 'AiyWuByWklrrUDlFignR',
+    url: 'https://zos.alipayobjects.com/rmsportal/',
+    linkUrl: 'https://www.baidu.com'
+  }
+]
+
+class Car extends React.Component {
+  state = {
+    initialHeight: 176
+  }
+
+  render () {
+    const hProp = this.state.initialHeight ? { height: this.state.initialHeight } : {}
+    return (
+      <div className={Style.myCarousel}>
+        <Carousel
+          className
+          autoplay
+          infinite
+          selectedIndex={1}
+          swipeSpeed={35}
+        >
+          {tempData.map(ii => (
+            <a href={ii.linkUrl} key={ii} style={hProp} className={Style.myCarouselLink}>
+              <img
+                className={Style.myCarouselImg}
+                src={`${ii.url}${ii.imgs}.png`}
+                alt=""
+                onLoad={() => {
+                  this.setState({
+                    initialHeight: null
+                  })
+                }}
+              />
+            </a>
+          ))}
+        </Carousel>
+      </div>
+    )
+  }
+}
 
 const CarouselCompoent = () => {
   return (
     // 根据父级传入的字段来循环出来
-    <div>1. 【走马灯】</div>
+    <div>
+      <Car />
+    </div>
   )
 }
+
 
 export default CarouselCompoent
