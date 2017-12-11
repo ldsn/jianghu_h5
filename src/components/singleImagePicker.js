59ce762435119a7d815b37d98951de4351bdbdd9
whistlemm <276158636@qq.com>
2017-12-11 20:42:56 +0800

    [feature] create article width banner

---
+++ src/components/singleImagePicker.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import Style from './singleImagePicker.less'
+
+class ImagePicker extends React.Component {
+  state = {
+    previewUrl: ''
+  }
+
+  constructor (props) {
+    super(props)
+  }
+
+  onFileChange = () => {
+    if (this.fileInput && this.fileInput.files.length) {
+      this.parseFile(this.fileInput.files[0])
+    }
+  }
+
+  parseFile (file) {
+    const reader = new FileReader()
+    reader.onload = e => {
+      const result = e.target.result
+
+      if (result) {
+        this.setState({
+          previewUrl: result
+        })
+        this.props.onChange({
+          file,
+          url: result
+        })
+      } else {
+        this.onFail('Fail to get image')
+      }
+    }
+    reader.readAsDataURL(file)
+  }
+
+  render () {
+    const props = this.props
+    const { previewUrl } = this.state
+    const previewProps = {
+      className: Style.preview,
+      style: {backgroundImage: `url(${previewUrl})`}
+    }
+
+    return (
+      <div
+        className={Style.wrapper}
+        role="button"
+        aria-label="Choose and add image"
+      >
+        <input
+          type="file"
+          accept="image/jpg,image/jpeg,image/png,image/gif"
+          ref={fileInput => this.fileInput = fileInput}
+          onChange={this.onFileChange}
+        />
+        {/* preview */}
+        <div
+          {...previewProps}
+        />
+        {/* placeholder */}
+        <div className={Style.placeholder}>
+          {props.placeholder}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ImagePicker
